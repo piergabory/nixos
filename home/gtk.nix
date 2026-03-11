c1087d49b3173f5b36a965a85dd85b837fd26085
Pierre Gabory <mail@piergabory.net>
2026-03-11 19:33:29 +0100

    GTK theming idk

--- home/gtk.nix
+++ home/gtk.nix
@@ -1,18 +1,44 @@
 { pkgs, ... }:
 
 {
+  home.packages = with pkgs; [
+    papirus-icon-theme
+    bibata-cursors
+    gnome-themes-extra
+    inter
+    inter-nerdfont
+  ];
+
   gtk = {
     enable = true;
+
     theme = {
-      name = "Adwaita-dark";
+      name = "Catppuccin-Mocha-Standard-Blue-Dark";
+      package = pkgs.catppuccin-gtk;
     };
+
     iconTheme = {
-      name = "Adwaita";
-      package = pkgs.adwaita-icon-theme;
+      name = "Morewaita-Dark";
+      package = pkgs.morewaita-icon-theme;
     };
+
     cursorTheme = {
-      name = "Adwaita";
-      package = pkgs.adwaita-icon-theme;
+      name = "Bibata-Modern-Ice";
+      package = pkgs.bibata-cursors;
+      size = 24;
+    };
+
+    font = {
+      name = "Inter";
+      size = 11;
+    };
+
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
     };
   };
 }
