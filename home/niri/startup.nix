7818ff0e6d6268c22db369bae510e62fb4110349
Pierre Gabory <mail@piergabory.net>
2026-03-11 19:37:05 +0100

    image background

--- home/niri/startup.nix
+++ home/niri/startup.nix
@@ -3,6 +3,6 @@
 {
   programs.niri.settings.spawn-at-startup = [
       { argv = [ "waybar" ]; }
-      { argv = [ "swaybg" "-c" "#000000" ]; }
+      { sh = [ "swaybg --image /home/piergabory/Pictures/wallpapers/tokyo-tower-skyline-night.jpg" ]; }
   ];
 }
