b22552ff185b4d596be2d35fe02835943e4ba577
Pierre Gabory <mail@piergabory.net>
2026-03-23 23:10:07 +0100

    Sync thing auth with agenix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,8 @@
 let
-  piergabory = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPQTeV5Wx7S0uHn3cn050h6xVRpXP867Mk2DoAK5qvd4";
   root = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAQ0bRf4yX3NHkM3hyccW2m+GBhPIUjOfkhKgZBeKkBv";
   system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAywPQ+IzZFizfeXYuz9PklOK1Wg9jGXkVSEjEYqZQ0B";
 in
 {
-  "samba-homeserver.age".publicKeys = [ piergabory root system ];
+  "samba-homeserver.age".publicKeys = [ root system ];
+  "syncthing-gui.age".publicKeys = [ root system ];
 }
--- services/syncthing.nix
+++ services/syncthing.nix
@@ -1,9 +1,27 @@
-{ ... }:
+{ config, ... }:
 
 {
+  age.secrets.syncthing = {
+    file = /etc/nixos/secrets/syncthing-gui.age;
+    mode = "0644";
+  };
+  
   services.syncthing = {
     enable = true;
     openDefaultPorts = true;
+    guiPasswordFile = config.age.secrets.syncthing.path;
+    settings = {
+      gui.user = "piergabory";
+      folder = {
+        "Documents" = {
+          path = /home/piergabory/Documents;
+          devices = [ "devices" ];
+        };
+      };
+      devices = {
+        "home-server".id = "XDBFUR4-FFBPR4G-JVVWYXJ-W2CFGNR-HF66NN7-U75MNVQ-U2WRMEP-R73RPQD";
+      };
+    };
   };
 
   networking.firewall.allowedTCPPorts = [ 8384 ];
