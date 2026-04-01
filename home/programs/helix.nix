f56e3580c7d6edfeaaea51b5314218aec2e90697
Pierre Gabory <mail@piergabory.net>
2026-04-02 01:16:48 +0200

    helix language server

--- home/programs/helix.nix
+++ home/programs/helix.nix
@@ -5,5 +5,12 @@
     enable = true;
 
     settings.theme = lib.mkForce "gruvbox_dark_hard";
+
+    languages.language = [
+      {
+        name = "rust";
+        formatter = { command = "rustfmt"; args = ["--edition" "2021"]; };
+      }
+    ];
   };
 }
