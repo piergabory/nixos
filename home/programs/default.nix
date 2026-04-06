5dff39635c615ff82ae81e2c888d8b2ab74cd437
Pierre Gabory <mail@piergabory.net>
2026-04-06 23:07:38 +0200

    revert to fuzzel

--- home/programs/default.nix
+++ home/programs/default.nix
@@ -8,6 +8,7 @@
     ./git.nix
     ./helix.nix
     ./kitty.nix
+    ./fuzzel.nix
     ./zen.nix
   ];
 }
--- home/programs/niri/bindings/programs.nix
+++ home/programs/niri/bindings/programs.nix
@@ -3,6 +3,6 @@
 {
   programs.niri.settings.binds = {
     "Mod+T".action.spawn = "kitty";
-    "Mod+Space".action.spawn = [ "sirula" ];
+    "Mod+Space".action.spawn = [ "fuzzel" ];
   };
 }
