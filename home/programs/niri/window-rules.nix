31063df41e4e19f1520d865bee931344cc745315
Pierre Gabory <mail@piergabory.net>
2026-04-07 00:22:35 +0200

    remove hack on zen

--- home/programs/niri/window-rules.nix
+++ home/programs/niri/window-rules.nix
@@ -5,17 +5,5 @@
     {
       draw-border-with-background = false;
     }
-
-    # Fix Zen browser border being strangely thin
-    {
-      matches = [ { app-id = "zen-beta"; } ];
-      border.width = 2;
-    }
-
-    {
-      matches = [ { app-id = "sirula"; } ];
-      open-floating = true;
-      border.enable = false;
-    }
   ];
 }
