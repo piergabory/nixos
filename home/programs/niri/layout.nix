199eb98e112151ffc91cb03c8286d0b1daf5df8b
Pierre Gabory <mail@piergabory.net>
2026-04-06 21:53:27 +0200

    Smaller panel gaps

--- home/programs/niri/layout.nix
+++ home/programs/niri/layout.nix
@@ -5,10 +5,10 @@
     struts = {
       left = 0;
       right = 0;
-      top = -24;
+      top = -10;
       bottom = 0;
     };
-    gaps = 24;
+    gaps = 10;
 
     default-column-width.proportion = 0.5;
     default-column-display = "normal";
