79d1f7ca55c01e05f34a988e10d8d2cdd6803123
Pierre Gabory <mail@piergabory.net>
2026-04-07 20:05:56 +0200

    refine zen pins

--- home/programs/browser/pins.nix
+++ home/programs/browser/pins.nix
@@ -15,7 +15,7 @@
       id = url;
     };
     "Youtube" = rec {
-      url = "https://www.youtube.com/subscriptions";
+      url = "https://www.youtube.com/feed/subscriptions";
       id = url;
     };
     "Reddit" = rec {
@@ -23,19 +23,19 @@
       id = url;
     };
     "Home Assistant" = rec {
-      url = "https://hass.piergabory.net";
+      url = "https://hass.piergabory.net/dashboard-areas/home";
       id = url;
     };
     "Jellyfin" = rec {
-      url = "https://jelly.piergabory.net";
+      url = "https://jelly.piergabory.net/web/#/home";
       id = url;
     };
     "Vault" = rec {
-      url = "https://vault.piergabory.net";
+      url = "https://vault.piergabory.net/#/vault";
       id = url;
     };
     "Photos" = rec {
-      url = "https://photo.piergabory.net";
+      url = "https://photo.piergabory.net/photos";
       id = url;
     };
     "Syncthing" = rec {
