d9e6d984210426a7b66cf9db7288a3fcc8819e49
Pierre Gabory <mail@piergabory.net>
2026-04-07 14:41:13 +0200

    wip zen profile

--- home/programs/zen.nix
+++ home/programs/zen.nix
@@ -1,9 +1,35 @@
-{ ... }:
+{ pkgs, inputs, ... }:
 
 {
-  programs.zen-browser.enable = true;
+  programs.zen-browser = {
+    enable = true;
+    nativeMessagingHosts = [ pkgs.firefoxpwa ];
+    policies = {
+      AutofillAddressEnabled = true;
+      AutofillCreditCardEnabled = false;
+      DisableAppUpdate = true;
+      DisableFeedbackCommands = true;
+      DisableFirefoxStudies = true;
+      DisablePocket = true;
+      DisableTelemetry = true;
+      DontCheckDefaultBrowser = true;
+      NoDefaultBookmarks = true;
+      OfferToSaveLogins = false;
+    };
 
-  # stylix.targets.zen-browser.profileNames = [ "Default Profile" ];
+    profiles.default = {
+      search = {
+        force = true;
+        default = "ddg"; #duck duck go
+      };
+      # extensions.packages = with inputs.firefox-addons.packages.${pkgs.stdenv.hostPlatform.system}; [
+      #   ublock-origin
+      #   bitwarden
+      # ];
+    };
+  };
+
+  stylix.targets.zen-browser.profileNames = [ "default" ];
 
   home.sessionVariables = {
     MOZ_ENABLE_WAYLAND = "1";
