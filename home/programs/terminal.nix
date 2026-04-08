16b366ae35cdc24f6ac89bd7b16ae3f55422a15b
Pierre Gabory <mail@piergabory.net>
2026-04-09 00:50:07 +0200

    distracted

--- home/programs/terminal.nix
+++ home/programs/terminal.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, ... }:
 
 {
   programs.foot = {
@@ -9,6 +9,48 @@
 
   programs.starship = {
     enable = true;
+    enableZshIntegration = true;
+    settings = {
+      format = lib.concatStrings [
+        "$username"
+        "$hostname"
+        "[](bg:blue fg:black)"
+        "$directory"
+        "[](bg:yellow fg:blue)"
+        "$git_branch"
+        "$git_status"
+      ];
+      username = {
+        show_always = true;
+        style_user = "green";
+        style_root = "red";
+        format = "[ $user](fg:$style bg:black bold)";
+        disabled = false;
+      };
+      hostname = {
+        ssh_only = false;
+        show_always = true;
+        style = "fg:white bg:black bold";
+        format = "[@$hostname ]($style)";
+        disabled = false;
+      };
+      directory = {
+        style = "bg:blue dimmed fg:black";
+        disabled = false;
+        format = "[ $path ]($style)";
+        truncation_length = 3;
+        truncation_symbol = "…/";
+      };
+      git_branch = {
+        symbol = "";
+        style = "bg:yellow fg:black";
+        format = "[ $symbol $branch]($style)";
+      };
+      git_status = {
+        style = "yellow";
+        format = "[ $all_status $ahead_behind](fg:black bg:$style)[ ](fg:$style bg:black)";
+      };
+    };
   };
 
   programs.zsh = {
@@ -22,7 +64,7 @@
 
     spawn-at-startup = [
       { sh = "foot --server"; }
-      { sh = "eval $(starship init zsh)"; }
+      # { sh = "eval $(starship init zsh)"; }
     ];
   };
 }
