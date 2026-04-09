cf59b45452881c5861a04975b879bd58a0948e5a
Pierre Gabory <mail@piergabory.net>
2026-04-10 00:10:56 +0200

    better ly font

--- modules/displaymanager.nix
+++ modules/displaymanager.nix
@@ -1,9 +1,15 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   services.displayManager = {
     ly.enable = true;
     defaultSession = "niri";
+  };
+  
+  console = {
+    font = "ter=v32b";
+    packages = [ pkgs.terminus_font ];
+    earlySetup = true;
   };
 
   security.pam.services.ly.fprintAuth = true;
