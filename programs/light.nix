eb19e8cab8f1c13a296bfc4ba994b92fa781f2ce
Pierre Gabory <mail@piergabory.net>
2026-04-14 23:39:07 +0200

    fix backlight

--- programs/light.nix
+++ programs/light.nix
@@ -1,28 +1,7 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-  # programs.light = {
-  #   enable = true;
-  #   brightnessKeys = {
-  #     enable = true;
-  #     minBrightness = 0;
-  #     step = 10;
-  #   };
-  # };
-
-  services.actkbd = {
-    enable = true;
-    bindings = [
-      {
-        keys = [ 374 ];
-        events = [ "key" ];
-        # Todo script looping because i think this doens't allow lowering the value
-        # BTW the minimum is 50
-        command = ''
-          /run/wrappers/bin/light -A 10 -s sysfs/leds/tpacpi::kbd_backlight
-        '';
-      }
-      # { keys = [ ??? ]; events = [ "key" ]; command = "/run/wrappers/bin/light -U 10"; }
-    ];
-  };
+  environment.systemPackages = with pkgs; [
+    brightnessctl
+  ];
 }
