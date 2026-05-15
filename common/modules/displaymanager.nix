0dfd9694ffecea0aebb2b60ecfdf60fdce4e19d4
Pierre Gabory <mail@piergabory.net>
2026-05-15 22:52:30 +0200

    fix font

--- common/modules/displaymanager.nix
+++ common/modules/displaymanager.nix
@@ -1,14 +1,8 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   services.displayManager = {
     ly.enable = true;
     defaultSession = "niri";
-  };
-  
-  console = {
-    font = "Lat2-Terminus16";
-    packages = [ pkgs.terminus_font ];
-    earlySetup = true;
   };
 }
