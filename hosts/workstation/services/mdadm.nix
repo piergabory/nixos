d0898db63f765bce88013a17ca10c309f30fc338
Pierre Gabory <mail@piergabory.net>
2026-07-02 09:36:14 +0200

    fix drive

--- hosts/workstation/services/mdadm.nix
+++ hosts/workstation/services/mdadm.nix
@@ -5,10 +5,34 @@
     mdadm # Software RAID for /storage
   ];
 
-  boot.swraid = {
-    enable = true;
-    mdadmConf = ''
-      MAILADDR mail@piergabory.net
-    '';
+  boot = {
+    initrd = {
+      kernelModules = [
+        "md_mod"
+        "raid1"
+      ];
+
+      systemd.services.assemble-storage-raid = {
+        description = "Assemble /storage RAID array";
+        wantedBy = [ "initrd.target" ];
+        before = [ "initrd-parse-etc.service" ];
+        after = [ "systemd-udev-trigger.service" ];
+
+        serviceConfig = {
+          Type = "oneshot";
+          RemainAfterExit = true;
+          ExecStartPre = "/bin/udevadm settle --timeout=10";
+          ExecStart = "/sbin/mdadm --assemble --scan --run";
+        };
+      };
+    };
+
+    swraid = {
+      enable = true;
+      mdadmConf = ''
+        ARRAY /dev/md/mac-pro-workstation:0 metadata=1.2 UUID=43cd6b10:25cf7256:b8ebe932:0e639d62
+        MAILADDR mail@piergabory.net
+      '';
+    };
   };
 }
