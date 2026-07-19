5e97ef6ff491ef3c719cea5d2723666bd72050d7
Pierre Gabory <mail@piergabory.net>
2026-07-19 22:49:09 +0200

    fix build nixvim duplicated

--- common/home/programs/zed-editor.nix
+++ common/home/programs/zed-editor.nix
@@ -12,11 +12,7 @@
       "git-firefly"
       "html"
     ];
-    theme = "Gruvbox dark";
     userSettings = {
-      buffer_font_size = 14;
-      buffer_font_family = "FiraCode Nerd Font";
-      ui_font_size = 14;
       vim_mode = true;
       feature = {
         copilot = true;
