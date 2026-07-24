8bb0a22ac702ca51d521da292b217a4aae5dd965
Pierre Gabory <mail@piergabory.net>
2026-07-24 22:29:52 +0200

    fix matter

--- services/homelab/home-assistant/matter-server.nix
+++ services/homelab/home-assistant/matter-server.nix
@@ -5,6 +5,12 @@ let
   cfg = config.services.hass-container;
 in {
   config = mkIf cfg.enable {
+    services.avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
+
     services.home-assistant-matter-hub = {
       enable = true;
       accessTokenFile = config.age.secrets.home-assistant-token.path;
@@ -18,6 +24,8 @@ in {
     services.matter-server = {
       enable = true;
       openFirewall = true;
+      # Matter devices advertise scoped IPv6 addresses on the wired LAN.
+      extraArgs.primary-interface = "eth0";
     };
   };
 }
