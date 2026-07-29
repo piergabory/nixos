3c496e3bb105a1af391909eaa5129dd7cdaa449a
Pierre Gabory <pierre.gabory@legrand.com>
2026-07-29 18:10:57 +0200

    add warp

--- home/developer/default.nix
+++ home/developer/default.nix
@@ -1,21 +1,25 @@
 { pkgs, ... }:
 
 {
-    imports = [
-      ./languages
-      ./git.nix
-      ./nixvim
-      ./zed-editor.nix
-      ./tokscale.nix
-    ];
+  imports = [
+    ./languages
+    ./git.nix
+    ./nixvim
+    ./zed-editor.nix
+    ./tokscale.nix
+  ];
 
-    config = {
-      programs.opencode = {
-        enable = true;
-        package = if pkgs.stdenv.hostPlatform.isDarwin then null else pkgs.opencode;
-      };
-      programs.github-copilot-cli.enable = true;
-      programs.helix.enable = true;
-      programs.tokscale.enable = true;
+  config = {
+    programs.opencode = {
+      enable = true;
+      package = if pkgs.stdenv.hostPlatform.isDarwin then null else pkgs.opencode;
     };
+    programs.github-copilot-cli.enable = true;
+    programs.helix.enable = true;
+    programs.tokscale.enable = true;
+
+    home.packages = with pkgs; [
+      warp-terminal
+    ];
+  };
 }
