723a75ef58b48431fe336f65463fe26a447a98a1
Pierre Gabory <pierre.gabory@legrand.com>
2026-09-08 10:11:12 +0200

    Fix duplicated firewall opening

--- modules/homelab/minecraft-server/default.nix
+++ modules/homelab/minecraft-server/default.nix
@@ -1,43 +1,49 @@
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 with lib;
 
 let
   cfg = config.services.minecraft-server;
-in {
+in
+{
   imports = [
     ./backup.nix
     ./whitelist.nix
     ./properties.nix
   ];
 
-  config = let
-    minecraftServerPackage = with pkgs; callPackage "${path}/pkgs/by-name/mi/minecraft-server/derivation.nix" {
-      jre_headless = jdk25_headless;
-      version = "26.1.2";
-      url = "https://piston-data.mojang.com/v1/objects/97ccd4c0ed3f81bbb7bfacddd1090b0c56f9bc51/server.jar";
-      sha1 = "97ccd4c0ed3f81bbb7bfacddd1090b0c56f9bc51";
-    };
-  in {
-    services.minecraft-server = mkIf cfg.enable {
-      eula = true;
-      openFirewall = true;
-      package = minecraftServerPackage;
-      dataDir = "/var/lib/minecraft";
-      declarative = true;
+  config =
+    let
+      minecraftServerPackage =
+        with pkgs;
+        callPackage "${path}/pkgs/by-name/mi/minecraft-server/derivation.nix" {
+          jre_headless = jdk25_headless;
+          version = "26.1.2";
+          url = "https://piston-data.mojang.com/v1/objects/97ccd4c0ed3f81bbb7bfacddd1090b0c56f9bc51/server.jar";
+          sha1 = "97ccd4c0ed3f81bbb7bfacddd1090b0c56f9bc51";
+        };
+    in
+    {
+      services.minecraft-server = mkIf cfg.enable {
+        eula = true;
+        openFirewall = true;
+        package = minecraftServerPackage;
+        dataDir = "/var/lib/minecraft";
+        declarative = true;
 
-      # 512MB to 4GB, clean up every 60ish seconds
-      jvmOpts = ''
-        -Xms512M \
-        -Xmx2G \
-        -XX:MaxHeapFreeRatio=30 \
-        -XX:MinHeapFreeRatio=10 \
-        -XX:+UseG1GC \
-        -XX:G1PeriodicGCInterval=60000
-      '';
+        # 512MB to 4GB, clean up every 60ish seconds
+        jvmOpts = ''
+          -Xms512M \
+          -Xmx2G \
+          -XX:MaxHeapFreeRatio=30 \
+          -XX:MinHeapFreeRatio=10 \
+          -XX:+UseG1GC \
+          -XX:G1PeriodicGCInterval=60000
+        '';
+      };
     };
-
-    networking.firewall.allowedTCPPorts = [
-      cfg.serverProperties.server-port
-    ];
-  };
 }
